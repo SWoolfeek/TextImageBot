8bae7388ce7a2974e3064114f12a511cb9b88142
Kyrylo Atamanenko <sacredwoolf@gmail.com>
2020-01-09 13:23:50 +0100

    Examples of keys.py

---
+++ keys.py
@@ -0,0 +1,6 @@
+telegram_token = '' # <== You telegram bot_token
+
+sql_user = '' # <== database user name ## 'admin'
+sql_password = '' # <== database password ## 'qwerty'
+sql_host = '' # <== database ip to connect # '192.168.0.0'
+sql_database = '' # <== database name ## 'textbot'
